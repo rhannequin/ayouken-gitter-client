964f554f6873fb0973c4f7ccf9c7ca6820fe9c59
Rémy Hannequin <remy.hannequin@gmail.com>
2014-02-27 00:48:40 +0100

    Important improvments + use proxy url + use SSL + use jsonp

--- index.js
+++ index.js
@@ -21,29 +21,79 @@ function Ayouken(win, doc, $) {
 
 
   function talk(text) {
-    if(text.slice(0, 8) === '@ayouken') {
-      var command = text.slice(9)
-        , message
-
-      if(commands.hasOwnProperty(command)) {
-        message = commands[command]()
-      } else {
-        message = "Don't understand what you mean by `" + command + "`, bro"
-      }
-
+    var render = function(message) {
       $textarea.val(message)
       $textarea.trigger($.Event('keydown', { keyCode: 13 }))
+    }
+
+    if(text.slice(0, 8) === '@ayouken') {
+      var command = text.slice(9)
+      if(commands.hasOwnProperty(command)) {
+        commands[command](render)
+      } else {
+        commands.unknown(render)
+      }
     }
   }
 
   function Commands() {
     var c = {}
+      , api = new Api()
 
-    c.roulette = function() {
-      return (Math.floor(Math.random() * 6 + 1) === 6) ? 'Bang!' : 'Click...'
+    c.roulette = function(cb) {
+      api.req('/roulette', 'GET', {}, function(res) {
+        cb(res.data)
+      })
     }
 
+    c.gif = function(cb) {
+      api.req('/gif', 'GET', {}, function(res) {
+        var data = res.data
+        cb(data.title + ': ' + data.link)
+      })
+    }
+
+    c.help = function(cb) {
+      api.req('/help', 'GET', {}, function(res) {
+        var list = res.data
+          , commands = []
+          , res
+        list.forEach(function(el) {
+          commands.push('| `' + el.command + '` | ' + el.description)
+        })
+        res = "| Command   |   Description\n|:-----------|:------------|\n"
+        res += commands.join("\n")
+        cb(res)
+      })
+    }
+
+    c.unknown = function(cb) {}
+
     return c
+  }
+
+  function Api() {
+    var api = {}
+      , url = 'https://localhost:7001'
+
+    api.req = function(endpoint, type, data, success, error) {
+      endpoint = endpoint || '/'
+      type = type || 'GET'
+      data = data || {}
+      success = success || function(){}
+      error = error || function(){}
+      var ajax = $.ajax({
+          url: url + endpoint
+        , type: type
+        , data: data
+        , dataType: 'jsonp'
+        , crossDomain: true
+      })
+      ajax.done(success)
+      ajax.fail(error)
+    }
+
+    return api
   }
 
   return ayouken
